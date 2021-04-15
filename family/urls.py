e29f0078cd168eca300e052fefc5df5ad6b474e3
siddharth-999 <siddharthrakholiya9@gmail.com>
2021-04-15 20:38:14 +0530

    update family relative api and crated user listing api

--- family/urls.py
+++ family/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework import routers
 
-from .views import LoginAPIView, UserViewSet, FamilyRelativeViewSet
+from .views import LoginAPIView, UserViewSet, FamilyRelativeViewSet, FamilyMemberViewSet
 
 app_name = 'family'
 
@@ -9,7 +9,8 @@ router = routers.DefaultRouter()
 router.register('user', UserViewSet, basename='user')
 router.register('family_relative', FamilyRelativeViewSet,
                 basename='family_relative')
-
+router.register('family_member', FamilyMemberViewSet,
+                basename='family_member')
 urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='login'),
 ]
